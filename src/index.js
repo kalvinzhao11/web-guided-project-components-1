b4af7887853fe1534b5f4ade20a6419be07debe6
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-08 18:31:33 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ import constants from './data/constants'
 const { open, close } = constants
 
 // TASK 2- Verify our imports using log statements
-console.log() // log the panelData
-console.log() // log the open arrow
-console.log() // log the close arrow
+console.log(panelData) // log the panelData
+console.log(open) // log the open arrow
+console.log(close) // log the close arrow
 
 
 // TASK 3- Comment out the div.panel from index.html and grab its parent element.
